7077eea3451987baae87682cdf77a1bdb6a7fbf2
Lukas Horak <lukasbhorak@gmail.com>
2019-07-16 10:34:56 -0400

    capybara

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ def open_asset(file_name)
 end
 
 # Only run on development (local) instances not on production, etc.
-unless Rails.env.development?
+unless Rails.env.development? or Rails.env.test?
   puts "Development seeds only (for now)!"
   exit 0
 end
